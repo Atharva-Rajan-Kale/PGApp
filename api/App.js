f90366e016b9ee16ba24a3e2ff24f1e2acd464ad
Atharva-Kale <atharvakale912@gmail.com>
2024-08-14 23:21:46 -0400

    new post commit

--- api/App.js
+++ api/App.js
@@ -3,14 +3,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
-
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
-
-app.listen(8080, () => {
+app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
+app.listen(8800, () => {
   console.log("Server is running");
 });
